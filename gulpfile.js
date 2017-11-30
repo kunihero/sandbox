c9f15585114caadf36f334ec256a36c0230767a2
kunihero <k.okamura@kunihero.com>
2017-11-30 17:01:43 +0900

    sasswoコンパイル

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var slim = require("gulp-slim");
- 
+var sass = require("gulp-sass");
+
 gulp.task('slim', function(){
   gulp.src('views/*.slim')
     .pipe(plumber())
@@ -12,8 +13,15 @@ gulp.task('slim', function(){
     }))
     .pipe(gulp.dest(""));
 });
- 
+
 gulp.task('default', function() {
   gulp.watch(['views/*.slim','views/includes/*.slim'],['slim']);
 });
 
+gulp.task("default2", function() {
+    gulp.watch("assets/scss/*.scss", function(){
+        gulp.src("assets/scss/*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("assets/css"));
+    })
+});
